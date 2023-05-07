e44cae143bd78c2013c551d717980ac52d29a781
Francis Beeson <leobeeson@gmail.com>
2023-05-07 15:01:06 +0100

    Add test for asserting that an indexed term has been removed, and move all RawStringMatcher tests to single test class.

--- tests/test_classification_pipeline.py
+++ tests/test_classification_pipeline.py
@@ -2,24 +2,26 @@ import pytest
 from src.matchers.raw_string_matcher import RawStringMatcher
 
 
-@pytest.fixture
-def matches():
-    return {"sample search query": 235}
+class TestRawStringMatcher:
 
+    @pytest.fixture
+    def matches(self):
+        return {"sample search query": 235}
 
-@pytest.fixture
-def raw_string_matcher(matches):
-    return RawStringMatcher(matches)
+    @pytest.fixture
+    def raw_string_matcher(self, matches):
+        return RawStringMatcher(matches)
 
+    def test_raw_string_matcher(self, raw_string_matcher):
+        assert raw_string_matcher.match("sample search query") == 235
 
-def test_raw_string_matcher(raw_string_matcher):
-    assert raw_string_matcher.match("sample search query") == 235
+    def test_raw_string_matcher_no_match(self, raw_string_matcher):
+        assert raw_string_matcher.match("no match") is None
 
+    def test_raw_string_matcher_add_query(self, raw_string_matcher):
+        raw_string_matcher.add_query("another sample search query", 357)
+        assert raw_string_matcher.match("another sample search query") == 357
 
-def test_raw_string_matcher_no_match(raw_string_matcher):
-    assert raw_string_matcher.match("no match") is None
-
-
-def test_raw_string_matcher_add_query(raw_string_matcher):
-    raw_string_matcher.add_query("another sample search query", 357)
-    assert raw_string_matcher.match("another sample search query") == 357
+    def test_raw_string_matcher_remove_query(self, raw_string_matcher):
+        raw_string_matcher.remove_query("sample search query")
+        assert raw_string_matcher.match("sample search query") is None
