9934354d9beba54cbbb6ff963077447ddee90420
Francis Beeson <leobeeson@gmail.com>
2023-05-07 14:50:08 +0100

    Add method add_query and make tests.test_raw_string_matcher_add_query pass.

--- src/matchers/raw_string_matcher.py
+++ src/matchers/raw_string_matcher.py
@@ -5,5 +5,10 @@ class RawStringMatcher:
     def __init__(self, query_index):
         self.query_index = query_index
 
+
     def match(self, text):
         return self.query_index.get(text, None)
+
+
+    def add_query(self, text, category):
+        self.query_index[text] = category
