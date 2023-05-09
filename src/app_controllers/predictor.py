d264991e80898eb89dd4c9b5b3b3fea948a03e68
Francis Beeson <leobeeson@gmail.com>
2023-05-09 22:07:45 +0100

    Create Predictor class to coordinate execution of classification and evaluating predictions.

--- src/app_controllers/predictor.py
+++ src/app_controllers/predictor.py
@@ -1,42 +1,64 @@
 import os
-import logging
 
 
 from src.classifiers.classification_pipeline import ClassificationPipeline
-from src.indexers.labelled_data_set import LabelledDataSet
+from src.data_handlers.labelled_data_reader import LabelledDataReader
+from src.app_controllers.scorer import Scorer
+from src.loggers.log_utils import setup_logger
 
 
-logger = logging.getLogger(__name__)
+logger = setup_logger(__name__)
 
 
 class Predictor:
 
-    def __init__(self) -> None:
+    def __init__(self, data_filepath: str = None, validate: bool = False) -> None:
         logger.info("Instantiating Predictor...")
-        self.load_indices()
-
-
-    def load_indices(self) -> None:
-        train_data_index = self.load_train_data_index()
-        validation_data_index = self.load_validation_data_index()
-        self.train_data_index = train_data_index
-        self.validation_data_index = validation_data_index
-
-
-    def load_train_data_index(self) -> None:
-        train_data_filepath = os.environ["PATH_TRAIN_DATA"]
-        train_data_indexer = LabelledDataSet(train_data_filepath)
-        train_data = train_data_indexer.index_data()
-        return train_data
-
-    def load_validation_data_index(self) -> None:
-        validation_data_filepath = os.environ["PATH_VALIDATION_DATA"]
-        validation_data_indexer = LabelledDataSet(validation_data_filepath)
-        validation_data = validation_data_indexer.index_data()
-        return validation_data
+        self.validate: bool = validate
+        self.data_filepath: str = data_filepath
+        self.data: dict[str, int] = self._load_data()
+        self.classification_pipeline: ClassificationPipeline = ClassificationPipeline(self._load_train_data())
+        self.predictions: dict[str, int] = {}
+        self.scorer: Scorer = None
+        self.f1_score: float = None
 
 
     def predict(self) -> int:
-        classification_pipeline = ClassificationPipeline(self.train_data_index)
-        category = classification_pipeline.classify("sample search query")
+        logger.info("Predicting queries...")
+        for query in self.data:
+            category: int = self.predict_query(query)
+            if category is None:
+                category = -1
+            self.predictions[query] = category
+        if self.validate:
+            f1_score: float = self.score()
+            logger.info(f"F1 score: {f1_score}")
+        return self.predictions
+
+
+    def predict_query(self, query: str) -> int:
+        category: int = self.classification_pipeline.classify(query)
         return category
+
+
+    def score(self) -> float:
+        self.scorer: Scorer = Scorer(self.predictions, self.data)
+        f1_score: float = self.scorer.calculate_f1_score()
+        self.f1_score: float = f1_score
+        return f1_score
+
+
+    def _load_data(self) -> dict[str, int]:
+        if self.validate or self.data_filepath is None:
+            self.data_filepath: str = os.environ["PATH_VALIDATION_DATA"]
+        #TODO: Load data from text file for unlabelled (test) data.
+        data_reader: LabelledDataReader = LabelledDataReader(self.data_filepath)    
+        labelled_data: dict[str, int] = data_reader.read_labelled_data()
+        return labelled_data
+
+
+    def _load_train_data(self) -> dict[str, int]:
+        data_filepath: str = os.environ["PATH_TRAIN_DATA"]
+        data_reader: LabelledDataReader = LabelledDataReader(data_filepath)
+        labelled_train_data: dict[str, int] = data_reader.read_labelled_data()
+        return labelled_train_data
